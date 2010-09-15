a005cb440a5178a49af81860b04fb31cce80f071
Colin Van Dyke <cvandyke@demosphere.com>
2010-09-14 20:36:58 -0400

    updated pending delete button

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -3,9 +3,9 @@ Bridge.controllers :base do
   get :index, :map => "/" do
     render "base/index"
   end
-  get :index, :map => "/s" do
-    render "base/search_index"
-  end
+  # get :_index, :map => "/s" do
+  #   render "base/search_index"
+  # end
 
   get :about, :map => '/about' do
     render "base/about"
