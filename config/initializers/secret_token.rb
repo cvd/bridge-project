5247a49b9648f65cd750640efcf29c85ede3c026
Colin Van Dyke <cvandyke@demosphere.com>
2011-04-05 19:31:36 -0400

    initial commit of bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridgeProject::Application.config.secret_token = 'ae09cded7256a92db86eb4683c31407289652e363b30e0b2418705e1df850a8d625df50fe2fca0ba7f9caa320f6c902085c41e44c96f4199501c7f322632b505'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BridgeProject::Application.config.session_store :cookie_store, :key => '_bridge-project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BridgeProject::Application.config.session_store :active_record_store
