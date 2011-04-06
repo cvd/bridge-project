a9dfc209d4266a304911a33785d4bdd4e418a654
Colin Van Dyke <cvandyke@demosphere.com>
2011-04-06 17:15:54 -0400

    updates services relation

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -1,4 +1,4 @@
 class ServiceType < ActiveRecord::Base
   has_many :service_service_types
-  has_many :service, :through => :service_service_types
+  has_many :services, :through => :service_service_types
 end
